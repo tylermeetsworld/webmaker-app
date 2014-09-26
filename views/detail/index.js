94fa3eb34bfa7934fe690b3f5e8564883328d8eb
Tyler Nguyen <tylermeetsworld@gmail.com>
2014-09-25 22:42:50 -0500

    Add changes to detail/index.js

--- views/detail/index.js
+++ views/detail/index.js
@@ -5,9 +5,6 @@ var view = require('../../lib/view');
 module.exports = view.extend({
     id: 'detail',
     template: require('./index.html'),
-    data: {
-        title: 'Detail',
-    },
     created: function () {
         var self = this;
 
@@ -17,6 +14,6 @@ module.exports = view.extend({
 
         // Bind app
         self.$data.app = app.data;
-        self.$data.app.title = app.data.title;
+        self.title = app.data.name;
     }
 });
